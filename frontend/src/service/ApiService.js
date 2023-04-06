37a9827d57835c310c4557d4849220d631db2a15
Vinca0121 <qqw7820@gmail.com>
2023-04-06 15:15:50 +0900

    ApiService.js 완료 - 테스트 O

--- frontend/src/service/ApiService.js
+++ frontend/src/service/ApiService.js
@@ -1,32 +1,84 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request){
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if (accessToken)
+    {
+        headers.append("Authorization","Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url:API_BASE_URL + api,
         method: method,
     };
+
     if(request){
         options.body = JSON.stringify(request);
     }
 
-    return fetch(options.url, options).then((response) => 
+    return fetch(options.url, options)
+    .then((response) => 
         response.json().then((json)=>{
             if(!response.ok){
                 return Promise.reject(json);
             }
             return json;
         })
-        .catch((error) =>{
-            console.log(error.status);
-            if(error.status === 403){
+    )        
+    .catch((error) =>{
+        console.log("Oops!");
+        console.log(error.status);
+        console.log("Oops!");
+        if(error.status === 403){
                 window.location.href = "/login";
             }
             return Promise.reject(error);
-        })
-    );
+        });
+}
+
+//로그인을 위한 API 서비스 메소드 signin
+export function signin(userDTO){
+    return call("/auth/signin","POST",userDTO)
+    .then((response) =>{
+        if(response.token){
+            //local 스토리지에 토큰 저장
+            localStorage.setItem("ACCESS_TOKEN",response.token);
+            //token이 존재하는 경우 todo화면으로 이동
+            window.location.href ="/";
+        }
+    })
+}
+
+// 회원 가입 요청
+export function signup(userDTO){
+    return call("/auth/signup","POST",userDTO)
+    .then((response) =>{
+        if(response.id){
+            window.location.href ="/";
+        }
+    })
+    .catch((error) =>{
+        console.log("Oops!");
+        console.log(error.status);
+        console.log("Oops!");
+        if(error.status === 403){
+                window.location.href = "/auth/signup";
+            }
+            return Promise.reject(error);  
+    });
+}
+
+// 로그아웃
+export function signout(){
+    //local 스토리지에서 토큰을 삭제
+    localStorage.setItem("ACCESS_TOKEN",null);
+    window.location.href="/";
 }
 
 //back24 front 14
